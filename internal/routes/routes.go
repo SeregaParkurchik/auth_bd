2cbdc59f1663e3220ee709383418120c2e4734e7
Sergey_Krasnow <serejjja3@yandex.ru>
2025-02-05 12:19:55 +0300

    обновил маршрутизацию

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,17 +9,19 @@ import (
 func InitRoutes(userHandler *handlers.UserHandler) *mux.Router {
 	api := mux.NewRouter()
 
+	// Открытые маршруты
 	api.HandleFunc("/register", userHandler.Register).Methods("POST")
 	api.HandleFunc("/login", userHandler.Login).Methods("POST")
 
-	authHandler := mux.NewRouter()
+	// Защищенные маршруты для авторизованных пользователей
+	authHandler := api.PathPrefix("/").Subrouter()
 	authHandler.Use(userHandler.AuthMiddleware)
-
 	authHandler.HandleFunc("/flat/create", userHandler.FlatCreate).Methods("POST")
 
-	moderatorHandler := mux.NewRouter()
+	// Защищенные маршруты только для модераторов
+	moderatorHandler := api.PathPrefix("/").Subrouter()
+	moderatorHandler.Use(userHandler.AuthMiddleware)
 	moderatorHandler.Use(userHandler.ModeratorOnly)
-
 	moderatorHandler.HandleFunc("/house/create", userHandler.HouseCreate).Methods("POST")
 
 	return api
